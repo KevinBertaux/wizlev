1def194a27788c6a15d150ef7028fc3d080d5aa4
Kevin BERTAUX <58264753+KevinBertaux@users.noreply.github.com>
2026-03-04 20:21:06 +0100

    feat(ui): lot 7 pass 1 self-host lexend and nunito fonts

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 /* eslint-disable no-console */
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import '@fontsource/lexend/latin-ext-400.css';
+import '@fontsource/lexend/latin-ext-600.css';
+import '@fontsource/lexend/latin-ext-700.css';
+import '@fontsource/nunito/latin-ext-700.css';
+import '@fontsource/nunito/latin-ext-800.css';
 import App from './App.vue';
 import router from './router';
 import './styles/base.css';
